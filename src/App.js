9921b070b0cc1fc429902be5a77c534475a81c85
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2022-06-18 14:59:14 +0100

    the basic css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <header className="text-center">
+        <img src="/fav.png" alt="NelDev Logo" />
+      </header>
+      <section className='container'>
+        <header className='text-center m-1'>
+          <h3>++ Crypto List ++</h3>
+        </header>
+      </section>
+    </>
   );
 }
 
